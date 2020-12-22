88b017119a2a4d7110824a6d97a56ad68f17acd9
Strongly Typed <stronglytypedguy@gmail.com>
2020-12-21 23:12:42 -0800

    added 5

---
+++ src/five.rs
@@ -0,0 +1,14 @@
+pub fn solver(input: &String) -> String {
+    panic!("Not implemented")
+}
+
+#[cfg(test)]
+mod tests {
+    // Note this useful idiom: importing names from outer (for mod tests) scope.
+    use super::*;
+
+    #[test]
+    fn test_five_solver() {
+        
+    }
+}
